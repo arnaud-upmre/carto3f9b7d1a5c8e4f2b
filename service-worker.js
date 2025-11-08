959b49b5970d9dd6f4277b3a5f8bea9b84f841be
arnaud-upmre <mage.superficie9z@icloud.com>
2025-11-08 22:07:44 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,58 @@
+const TILE_CACHE = 'tiles-pro-v1';
+const MAX_TILES = 3000;
+
+const CACHE_DOMAINS = [
+  'openrailwaymap.org',
+  'tile.openstreetmap.org',
+  'data.geopf.fr',
+  '/MapServer/'  // Esri
+];
+
+
+async function limitCacheSize(cacheName, maxItems) {
+  const cache = await caches.open(cacheName);
+  const keys = await cache.keys();
+
+  if (keys.length > maxItems) {
+    const excess = keys.length - maxItems;
+
+    for (let i = 0; i < excess; i++) {
+      await cache.delete(keys[i]);
+    }
+  }
+}
+
+
+self.addEventListener('fetch', event => {
+  const url = event.request.url;
+
+  if (!CACHE_DOMAINS.some(domain => url.includes(domain))) {
+    return;
+  }
+
+  event.respondWith(
+    caches.open(TILE_CACHE).then(async cache => {
+
+
+      const cached = await cache.match(event.request);
+      if (cached) return cached;
+
+ 
+      try {
+        const response = await fetch(event.request, { mode: 'no-cors' });
+
+  
+        cache.put(event.request, response.clone());
+
+  
+        limitCacheSize(TILE_CACHE, MAX_TILES);
+
+        return response;
+
+      } catch (err) {
+        // Offline total → on renvoie le cache si dispo
+        return cached || Response.error();
+      }
+    })
+  );
+});
